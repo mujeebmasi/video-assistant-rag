99d7173d54f877634d63ebc8cb9de7602dac435e
Shaikh Abdul Mujeeb Masi <shaikhabdulmujeeb0415@gmail.com>
2026-05-31 18:46:27 +0530

    modified test.py

--- test.py
+++ test.py
@@ -4,4 +4,5 @@ from core.transcriber import transcribe_all_chunks
 source = "https://www.youtube.com/watch?v=BedAaB1RKgE"
 
 chunks = process_input(source)
+print("Transcribing Started...")
 print(transcribe_all_chunks(chunks))
